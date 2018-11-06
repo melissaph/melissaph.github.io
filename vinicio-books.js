bf5188ce06c0c04a25b8066a6ad0a862fa7c276d
Melissa Patxot <melissapatxot@gmail.com>
2018-11-06 16:49:44 -0500

    Trial6

--- vinicio-books.js
+++ vinicio-books.js
@@ -53,7 +53,7 @@ $("#sum2").click(function(){
 
 	if(flag2 == true){
 	$("#span2").html("")
-	$("#book3").css("marginTop", "52%")
+	$("#book3").css("marginTop", "7%")
 		flag2 = false
 	}else{
 		$("#span2").html("⌄")
